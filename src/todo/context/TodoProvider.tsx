9aa99ddeeacaa9602f815d5f7d9999dc1841b309
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2021-12-20 16:47:24 -0300

    start TodoProvider

--- src/todo/context/TodoProvider.tsx
+++ src/todo/context/TodoProvider.tsx
@@ -1,11 +1,42 @@
 import { TodoContext } from "./TodoContext";
+import { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+import { TodoState } from "../interfaces/interfaces";
+
+const INITIAL_STATE: TodoState = {
+  todoCount: 2,
+  todos: [
+    {
+      id: "1",
+      description: "Recolectar las piedras del infinito",
+      completed: false,
+    },
+    {
+      id: "2",
+      description: "Piedra del alma",
+      completed: false,
+    },
+  ],
+  completed: 0,
+  pending: 2,
+};
 
 interface props {
   children: JSX.Element | JSX.Element[];
 }
 
 const TodoProvider = ({ children }: props) => {
-  return <TodoContext.Provider value={{}}>{children}</TodoContext.Provider>;
+  const [todoState, dispatch] = useReducer(todoReducer, INITIAL_STATE);
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todoState,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
 };
 
 export default TodoProvider;
